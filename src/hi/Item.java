af79cd7a8911f90f80552f06b7ea134eb7637573
Sergey Kovalevskyi <uasergostr@gmail.com>
2019-12-15 17:13:12 +0200

    Extracted Item inner class

---
+++ src/hi/Item.java
@@ -0,0 +1,42 @@
+package hi;
+
+/** item info */
+class Item
+{
+    private String title;
+    private double price;
+    private int quantity;
+    private ShoppingCart.Type type;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public ShoppingCart.Type getType() {
+        return type;
+    }
+
+    public void setType(ShoppingCart.Type type) {
+        this.type = type;
+    }
+}
--- src/hi/ShoppingCart.java
+++ src/hi/ShoppingCart.java
@@ -185,46 +185,7 @@ public class ShoppingCart
         }
         return discount;
     }
-    /** item info */
-    private static class Item
-    {
-        private String title;
-        private double price;
-        private int quantity;
-        private Type type;
 
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public double getPrice() {
-            return price;
-        }
-
-        public void setPrice(double price) {
-            this.price = price;
-        }
-
-        public int getQuantity() {
-            return quantity;
-        }
-
-        public void setQuantity(int quantity) {
-            this.quantity = quantity;
-        }
-
-        public Type getType() {
-            return type;
-        }
-
-        public void setType(Type type) {
-            this.type = type;
-        }
-    }
     /** Container for added items */
     private List<Item> items = new ArrayList<Item>();
 }
